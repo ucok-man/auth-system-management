ab3346040d6fb166f48fc3f555b26b4054597f41
ucok-man <ucokkocu411@gmail.com>
2025-12-16 23:45:13 +0700

    feat: add iam module, hashing service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { CoffeesModule } from './coffees/coffees.module';
 import { PrismaService } from './prisma.service';
 import { UsersModule } from './users/users.module';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [CoffeesModule, UsersModule],
+  imports: [CoffeesModule, UsersModule, IamModule],
   providers: [PrismaService],
 })
 export class AppModule {}
