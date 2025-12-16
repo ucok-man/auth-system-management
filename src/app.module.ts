add3fa1d940970e3b069d89146b6af8b09c4bed5
ucok-man <ucokkocu411@gmail.com>
2025-12-16 23:37:40 +0700

    feat: integrating db and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { CoffeesModule } from './coffees/coffees.module';
+import { PrismaService } from './prisma.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CoffeesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CoffeesModule, UsersModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
 
 @Module({
   controllers: [CoffeesController],
